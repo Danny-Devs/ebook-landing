20bd74b1fbc780f5fb6416596629c527558baf3f
Daniel Ahn <et3rnal.d@gmail.com>
2025-02-19 16:53:56 -0800

    remove sendgrid; add resend

--- src/routes/api/purchase-confirmation/+server.js
+++ src/routes/api/purchase-confirmation/+server.js
@@ -1,34 +1,49 @@
 import Stripe from 'stripe';
 import { json } from '@sveltejs/kit';
-import { STRIPE_API_KEY } from '$env/static/private';
+import {
+  STRIPE_API_KEY,
+  SENDGRID_API_KEY
+} from '$env/static/private';
+import resend from 'resend';
 
 const stripe = new Stripe(STRIPE_API_KEY);
+const resend = new Resend(SENDGRID_API_KEY);
 
 export async function POST({ request }) {
-  const requestBody = await request.json();
-  console.log(requestBody);
+  const sig = request.headers.get('stripe-signature');
+  const body = await request.text();
 
-  return json({ success: true });
-  
-  
-  // const sig = request.headers.get('stripe-signature');
-  // const body = await request.text();
+  let event;
 
-  // let event;
+  try {
+    event = stripe.webhooks.constructEvent(body, sig, STRIPE_API_KEY);
+  } catch (err) {
+    console.error(`Webhook signature verification failed: ${err.message}`);
+    return json(
+      { error: 'Webhook signature verification failed' },
+      { status: 400 }
+    );
+  }
 
-  // try {
-  //   event = stripe.webhooks.constructEvent(body, sig, 'your-webhook-signing-secret');
-  // } catch (err) {
-  //   console.error(`Webhook signature verification failed: ${err.message}`);
-  //   return json({ error: 'Webhook signature verification failed' }, { status: 400 });
-  // }
+  if (event.type === 'checkout.session.completed') {
+    const session = event.data.object;
+    // Handle the checkout session completion by sending email via SendGrid
+    const msg = {
+      from: "et3rnal.d@gmail.com",
+      to: session.customer_details.email,
+      subject: 'Purchase Confirmation',
+      html: `<strong>Thank you for your purchase!</strong><br>Your session ID is ${session.id}.`
+    };
 
-  // if (event.type === 'checkout.session.completed') {
-  //   const session = event.data.object;
-  //   // Handle the checkout session completion
-  //   console.log(`Payment for session ${session.id} was successful!`);
-  //   // You can perform additional actions here, such as updating your database
-  // }
+    try {
+      await resend.emails.send(msg);
+      console.log(
+        `Payment for session ${session.id} was successful! Confirmation email sent.`
+      );
+    } catch (error) {
+      console.error(`Error sending confirmation email: ${error.message}`);
+    }
+  }
 
-  // return json({ received: true }, { status: 200 });
+  return json({ received: true }, { status: 200 });
 }
